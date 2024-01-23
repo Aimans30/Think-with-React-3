0d5a1df657549ee6c2badf204fa7bb90e60770c0
Aimans30 <aiman.singh@kalvium.community>
2024-01-23 12:13:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/HelloKalvium.jsx
+
+import React from 'react';
+import './HelloKalvium.css'; // Import the CSS file for styling
+
+const HelloKalvium = () => {
+  return (
+    <div className="hello-kalvium">
+      <h1>Hello Kalvium</h1>
+    </div>
+  );
+};
+
+export default HelloKalvium;
